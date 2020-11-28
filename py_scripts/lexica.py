Fixed error in calculation of mutual_utility.

py_scripts/lexica.py
@@ -68,8 +68,6 @@ def get_lexica_bins(lexica_list, all_states):
         concept_indices = []
         current_lex = np.transpose(lex) 
         for concept in current_lex:
-            if np.array_equal(concept, np.array([-100, -100, -100])):
-                continue
             concept_indices.append(concepts.index(tuple(concept)))
         lexica_concepts.append(concept_indices)
 
@@ -133,6 +131,7 @@ def get_prior(lexica_list, cost, all_states):
         concepts += list(product([0,1], repeat=state))
         concepts.remove(tuple(np.zeros(state)))  # remove message false of all states
         concepts.remove(tuple(np.ones(state)))  # remove message true of all states
+
     if cost == "brochhagen": # cost of each concept in 'concepts'
         cost_dict = {(0,0,1): 3, (0,1,0):8, (0,1,1):4, (1,0,0):4, (1,0,1):10, (1,1,0):5, # for three states
                     (0,1):3, (1,0):4}  # for two states
@@ -149,10 +148,6 @@ def get_prior(lexica_list, cost, all_states):
         current_lex = np.transpose(lex)
         lex_val = 1  # probability of current lexicon's concepts
         for concept in current_lex:
-
-            if np.array_equal(concept, np.array([-100, -100, -100])):
-                continue
-
             lex_val *= cost_dict[tuple(concept)]
         out.append(lex_val)
     out = out + out  # double for two types of linguistic behavior

py_scripts/mutation_matrix.py
@@ -12,23 +12,6 @@ import os.path
 
 """Calculates the mutation_matrix"""
 
-def pad_lexicon(l, states, messages, pad_value=-100):
-    """adds pads to the lexicon
-
-    :param l: lexicon
-    :type l: np.array
-    :param states: number of desired states after padding
-    :type states: int
-    :param messages: number of desired messages after padding
-    :type messages: int
-    :param pad_value: value used for padding, default=-100
-    :type pad_value: int
-    :return: padded lexicon
-    :rtype: np.array
-    """
-    return np.pad(l, [(0, states - l.shape[0]),(0,messages - l.shape[1])], mode='constant', constant_values=pad_value) 
-
-
 def normalize(m):
     return m / m.sum(axis=1)[:, np.newaxis]
 
@@ -47,19 +30,14 @@ def summarize_counts(lst,states,messages):
     """
     counter = [0 for _ in range(states*messages)]
     for i in range(len(lst)):
-        s,m = lst[i][0] *messages, lst[i][1]
+        s,m = lst[i][0] * messages, lst[i][1]
         counter[s+m] += 1
     return counter
 
-def get_obs(s_amount,m_amount,k,likelihoods,sample_amount):
+def get_obs(all_states, all_messages, k,likelihoods,sample_amount):
     """Returns summarized counts of k-length <s_i,m_j> production observations as [#(<s_0,m_0>), #(<s_0,m_1), #(<s_1,m_0>, #(s_1,m_1)], ...]] = k
     get production data from all types
 
-    :param s_amount: number of states
-    :type s_amount: int
-    :param m_amount: number of messages
-    :type m_amount: int
-    :param k: sequence length k
     :type k: int
     :param likelihoods: list containig likelihood-lists (sender matrices)
     :type likelihoods: np.array
@@ -68,42 +46,29 @@ def get_obs(s_amount,m_amount,k,likelihoods,sample_amount):
     :return: list containing lists with numbers of sampled sm-pairs ([0,4,0,2], [3,4,3,2]) first sample 0 s_0,m_0, 4 s_1, m_1
     :rtype: list
     """
+        
+    obs = [] #store all produced k-length (s,m) sequences 
 
-    if k == 1 and s_amount == 3 and m_amount == 3: #If k = 1 then no sampling, compute Q with full set of possible observations
-        all_data = [[0,0,0,0,0,0,0,0,1],\
-                [0,0,0,0,0,0,0,1,0],\
-                [0,0,0,0,0,0,1,0,0],\
-                [0,0,0,0,0,1,0,0,0],\
-                [0,0,0,0,1,0,0,0,0],\
-                [0,0,0,1,0,0,0,0,0],\
-                [0,0,1,0,0,0,0,0,0],\
-                [0,1,0,0,0,0,0,0,0],\
-                [1,0,0,0,0,0,0,0,0]]
-        return [all_data for _ in range(432)] #all data for each parent type possible
-    else:
-          
-        obs = [] #store all produced k-length (s,m) sequences 
+    for t in range(len(likelihoods)): # iterating over types
+        produced_obs = [] #store k-length (s,m) sequences of a type
+        type_m_amount = likelihoods[t].shape[1] # get size of type for doubled_state_freq
+        type_s_amount = likelihoods[t].shape[0] 
+        s = list(range(type_s_amount))
+        m = list(range(type_m_amount))
+        atomic_observations = list(product(s,m)) # all possible state message pairs
+        production_vector = likelihoods[t].flatten()
+        state_freq = np.ones(type_s_amount) / float(type_s_amount) #frequency of states s_1,...,s_n 
+        doubled_state_freq = np.column_stack(tuple(state_freq for _ in range(type_m_amount))).flatten() # P(s)
+        sample_vector = production_vector * doubled_state_freq # P(m|s,t_i) * P(s)
 
-        for t in range(len(likelihoods)): # iterating over types
-            produced_obs = [] #store k-length (s,m) sequences of a type
-            type_m_amount = likelihoods[t].shape[1] # get size of type for doubled_state_freq
-            type_s_amount = likelihoods[t].shape[0] 
-            s = list(range(type_s_amount))
-            m = list(range(type_m_amount))
-            atomic_observations = list(product(s,m)) # all possible state message pairs
-            production_vector = likelihoods[t].flatten()
-            state_freq = np.ones(type_s_amount) / float(type_s_amount) #frequency of states s_1,...,s_n 
-            doubled_state_freq = np.column_stack(tuple(state_freq for _ in range(type_m_amount))).flatten() # P(s)
-            sample_vector = production_vector * doubled_state_freq # P(m|s,t_i) * P(s)
+        for _ in range(sample_amount):
+            sampled_idx = [np.random.choice(range(len(atomic_observations)),p=sample_vector) for _ in range(k)] #sample state_message pair
+            sampled_obs = [atomic_observations[x] for x in sampled_idx]
+            produced_obs.append(summarize_counts(sampled_obs, max(all_states), max(all_messages)))
+        obs.append(produced_obs)
+    return obs
 
-            for i in range(sample_amount):
-                sampled_idx = [np.random.choice(range(len(atomic_observations)),p=sample_vector) for _ in range(k)] #sample state_message pair
-                sampled_obs = [atomic_observations[x] for x in sampled_idx]
-                produced_obs.append(summarize_counts(sampled_obs, type_s_amount, type_m_amount))
-            obs.append(produced_obs)
-        return obs
-
-def get_likelihood(obs,likelihoods):
+def get_likelihood(all_states, all_messages, obs,likelihoods):
     """P(parent data|t_i) for all types and its samples
 
     :param obs: summarized counts of sampled ms-pairs for a type
@@ -116,11 +81,26 @@ def get_likelihood(obs,likelihoods):
     out = np.zeros([len(likelihoods), len(obs)]) # matrix to store results in
     for lhi in range(len(likelihoods)):
         flat_lhi = likelihoods[lhi].flatten()
+
         for o in range(len(obs)):
-            if len(flat_lhi) < len(obs[0]): #TODO what if different shape
-                out[lhi,o] = np.prod([flat_lhi[x]**obs[o][x] for x in range(len(flat_lhi))])
-            else:
-                out[lhi,o] = np.prod([flat_lhi[x]**obs[o][x] for x in range(len(obs[o]))])
+            max_messages = max(all_messages)
+            type_messages = likelihoods[lhi].shape[1]
+            if type_messages < max_messages:
+                m_counter = 1
+                prod = []
+                index = 0
+                for x in range(len(obs[o])):
+                    if m_counter <= type_messages:
+                        prod.append(flat_lhi[index]**obs[o][x])
+                        index += 1
+                        m_counter += 1
+                    elif m_counter < max_messages:
+                        m_counter += 1
+                    elif m_counter == max(all_messages):
+                        m_counter = 1
+                out[lhi,o] =  np.prod(prod) 
+            else: 
+                out[lhi,o] = np.prod([flat_lhi[x_i]**obs[o][x_i] for x_i in range(len(obs[o]))])
     return out
 
 def get_mutation_matrix(s_amount,m_amount, likelihoods,lexica_prior,learning_parameter,sample_amount,k,lam,alpha,mutual_exclusivity, result_path, predefined):
@@ -134,13 +114,12 @@ def get_mutation_matrix(s_amount,m_amount, likelihoods,lexica_prior,learning_par
                %(result_path, s_amount,str(m_amount),lam,alpha,k,sample_amount,learning_parameter,str(mutual_exclusivity)), delimiter=',')
     else:
         print('# Computing mutation matrix, ', datetime.datetime.now().replace(microsecond=0))
-        obs = get_obs(s_amount,max(m_amount),k,likelihoods,sample_amount) # get production data from all types
+        obs = get_obs(s_amount, m_amount, k,likelihoods,sample_amount) # get production data from all types
         out = np.zeros([len(likelihoods),len(likelihoods)]) #matrix to store Q # len(likelihoods) = type amount
 
         for parent_type in tqdm(range(len(likelihoods))):
             type_obs = obs[parent_type] #Parent production data
-            lhs = get_likelihood(type_obs,likelihoods) #P(parent data|t_i) for all types
-
+            lhs = get_likelihood(s_amount, m_amount, type_obs,likelihoods) #P(parent data|t_i) for all types
             post = normalize(lexica_prior * np.transpose(lhs)) #P(t_j|parent data) for all types; P(t_j)*P(d|t_j)
             parametrized_post = normalize(post**learning_parameter)
             normed_lhs = lhs[parent_type] / np.sum(lhs[parent_type]) # norm P(parent_data|parent_type)

py_scripts/mutual_utility.py
@@ -30,7 +30,7 @@ def pad_lexicon(l, states, messages, pad_value=-100):
     """
     return np.pad(l, [(0, states - l.shape[0]),(0,messages - l.shape[1])], mode='constant', constant_values=pad_value) 
 
-def get_utils(typeList,states,messages,lam,alpha,mutual_exclusivity, result_path, predefined):
+def get_utils(typeList, all_messages, all_states, lam,alpha,mutual_exclusivity, result_path, predefined):
     """calculates expected utiliy of types
 
     :param typeList: list containing already instantiated types of players
@@ -49,25 +49,27 @@ def get_utils(typeList,states,messages,lam,alpha,mutual_exclusivity, result_path
     :rtype: np.array
     """
 
-    if os.path.isfile('experiments/%s/matrices/umatrix-s%s-m%s-lam%d-a%d-me%s.csv' %(result_path, str(states),str(messages),lam,alpha,str(mutual_exclusivity))) and not predefined:
+    if os.path.isfile('experiments/%s/matrices/umatrix-s%s-m%s-lam%d-a%d-me%s.csv' %(result_path, str(all_states),str(all_messages),lam,alpha,str(mutual_exclusivity))) and not predefined:
         print('# Loading utilities,\t\t', datetime.datetime.now().replace(microsecond=0))
-        return np.genfromtxt('experiments/%s/matrices/umatrix-s%s-m%s-lam%d-a%d-me%s.csv' %(result_path, str(states),str(messages),lam,alpha,str(mutual_exclusivity)),delimiter=',')
+        return np.genfromtxt('experiments/%s/matrices/umatrix-s%s-m%s-lam%d-a%d-me%s.csv' %(result_path, str(all_states),str(all_messages),lam,alpha,str(mutual_exclusivity)),delimiter=',')
     else:
         print('# Computing utilities, ', datetime.datetime.now().replace(microsecond=0))
         out = np.zeros([len(typeList), len(typeList)])
         for i in tqdm(range(len(typeList))):
             for j in range(len(typeList)):
-                pad_sender_i = pad_lexicon(typeList[i].sender_matrix, states, messages, 1)
-                pad_sender_j = pad_lexicon(typeList[j].sender_matrix, states, messages, 1)
+                states = max([typeList[i].sender_matrix.shape[0],typeList[j].sender_matrix.shape[0]])             
+                messages = max([typeList[i].sender_matrix.shape[1],typeList[j].sender_matrix.shape[1]])
+                pad_sender_i = pad_lexicon(typeList[i].sender_matrix, states, messages, 0)
+                pad_sender_j = pad_lexicon(typeList[j].sender_matrix, states, messages, 0)
 
-                pad_receiver_i = np.transpose(pad_lexicon(np.transpose(typeList[i].receiver_matrix), states, messages, 1)) #TODO mit was padden?
-                pad_receiver_j = np.transpose(pad_lexicon(np.transpose(typeList[j].receiver_matrix), states, messages, 1))
+                pad_receiver_i = np.transpose(pad_lexicon(np.transpose(typeList[i].receiver_matrix), states, messages, 0)) 
+                pad_receiver_j = np.transpose(pad_lexicon(np.transpose(typeList[j].receiver_matrix), states, messages, 0))
 
-                #print("PAD SENDER I", pad_sender_i)
-                #print("PAD SENDER J", pad_sender_j)
+                
                 out[i,j] = (np.sum(pad_sender_i * np.transpose(pad_receiver_j))  / states 
                             + np.sum(pad_sender_j * np.transpose(pad_receiver_i))/ states ) / 2 
-            #raise Exception
+
+
     if not os.path.isdir("experiments/" + result_path +"/" + "matrices/"):
         os.makedirs("experiments/" + result_path + "/" + "matrices/" )
     with open("experiments/%s/matrices/umatrix-s%s-m%s-lam%d-a%d-me%s.csv" %(result_path, str(states), str(messages),lam,alpha,str(mutual_exclusivity)), "w") as file:

py_scripts/rmd.py
@@ -54,7 +54,7 @@ def get_target_bins(type_idx, bins, level, number_of_lexica):
 
 
 def run_dynamics(alpha,lam,k,sample_amount,gens,runs,learning_parameter,kind,mutual_exclusivity, result_path, 
-                cost, target_lex, target_level, competitor_lex, competitor_level, predefined = False, states=0, messages=0, print_x=6):
+                cost, target_lex, target_level, competitor_lex, competitor_level, predefined = False, all_states=0, all_messages=0, print_x=6):
 
     """[Runs the replicator mutator dynamics
     :param alpha: rate to control difference between semantic and pragmatic violations
@@ -96,13 +96,11 @@ def run_dynamics(alpha,lam,k,sample_amount,gens,runs,learning_parameter,kind,mut
     :param print_x: number of best x types to print in the results
     :type: int
     """
-    all_messages = messages
-    all_states = states 
 
     if not predefined:
-        if len(target_lex[0]) not in all_messages or len(target_lex) not in states:
+        if len(target_lex[0]) not in all_messages or len(target_lex) not in all_states:
             raise Exception("Target type does not fit the number of states and messages!")
-        if len(competitor_lex[0]) not in all_messages or len(competitor_lex) not in states:
+        if len(competitor_lex[0]) not in all_messages or len(competitor_lex) not in all_states:
             raise Exception("Competitor type does not fit the number of states and messages!")
 
 
@@ -111,34 +109,31 @@ def run_dynamics(alpha,lam,k,sample_amount,gens,runs,learning_parameter,kind,mut
 
     print('# Starting,\t\t\t', datetime.datetime.now().replace(microsecond=0))
     lexica, target_index, competitor_index = [], [], []
+
     if predefined: 
+        all_messages = set()
+        all_states = set()
         lexica = get_predefined_lexica(predefined)
         for index, l in enumerate(lexica): 
             s = np.array(l).shape[0]
             m = np.array(l).shape[1]
-
-            if s > states:
-                states = s
-            if m > messages:
-                messages = m
+            all_messages.add(m)
+            all_states.add(s)
             if np.array_equal(l, target_lex):
                 target_index = [index]
             if np.array_equal(l, competitor_lex):
                 competitor_index = [index]
+        all_messages = list(all_messages)
+        all_states = list(all_states)
 
-        all_messages = messages
- 
+
     else: 
-        for message in messages:
-            for state in states:
+        for message in all_messages:
+            for state in all_states:
                 lexicon, target_in, competitor_in = get_lexica(state, message, target_lex, competitor_lex, mutual_exclusivity)
                 lexica += lexicon
                 target_index += target_in
                 competitor_index += competitor_in
-
-
-        messages = max(messages)
-        states = max(states)
 
     bins = get_lexica_bins(lexica, all_states) #To bin types with identical lexica
     target_bins = get_target_bins(target_index[0], bins, target_level, len(lexica))
@@ -147,23 +142,23 @@ def run_dynamics(alpha,lam,k,sample_amount,gens,runs,learning_parameter,kind,mut
     l_prior = get_prior(lexica, cost, all_states)
     typeList = [LiteralPlayer(lam,lex) for lex in lexica] + [GriceanPlayer(alpha,lam,lex) for lex in lexica]
     likelihoods = [t.sender_matrix for t in typeList]
+
     
-    u = get_utils(typeList,states,messages,lam,alpha,mutual_exclusivity, result_path, predefined)
+    u = get_utils(typeList, all_messages, all_states, lam,alpha,mutual_exclusivity, result_path, predefined)
     q = get_mutation_matrix(all_states, all_messages,likelihoods,l_prior,learning_parameter,sample_amount,k,lam,alpha,mutual_exclusivity, result_path, predefined)
     print('# Beginning multiple runs,\t', datetime.datetime.now().replace(microsecond=0))
 
     if not os.path.isdir("experiments/" + result_path + "/results/"):
         os.makedirs("experiments/" + result_path + "/" +"results/")
-    f = csv.writer(open('experiments/%s/results/%s-s%s-m%s-lam%d-a%d-k%d-samples%d-l%d-g%d-me%s.csv' %(result_path, kind,str(states),str(messages),lam,alpha,k,sample_amount,learning_parameter,gens,str(mutual_exclusivity)),'w'))
+    f = csv.writer(open('experiments/%s/results/%s-s%s-m%s-lam%d-a%d-k%d-samples%d-l%d-g%d-me%s.csv' %(result_path, kind,str(all_states),str(all_messages),lam,alpha,k,sample_amount,learning_parameter,gens,str(mutual_exclusivity)),'w'))
     f.writerow(['runID','kind']+['t_ini'+str(x) for x in range(len(typeList))] +\
                ['lam', 'alpha','k','samples','l','gens', 'm_excl'] + ['t_final'+str(x) for x in range(len(typeList))])
     
 
-
-    if os.path.isfile('experiments/%s/results/00mean-%s-s%s-m%s-g%d-r%d-me%s.csv' %(result_path, kind,str(states),str(messages),gens,runs,str(mutual_exclusivity))):
-        f_mean = csv.writer(open('experiments/%s/results/00mean-%s-s%s-m%s-g%d-r%d-me%s.csv' %(result_path, kind,states,str(messages),gens,runs,str(mutual_exclusivity)), 'a'))
+    if os.path.isfile('experiments/%s/results/00mean-%s-s%s-m%s-g%d-r%d-me%s.csv' %(result_path, kind,str(all_states),str(all_messages),gens,runs,str(mutual_exclusivity))):
+        f_mean = csv.writer(open('experiments/%s/results/00mean-%s-s%s-m%s-g%d-r%d-me%s.csv' %(result_path, kind,str(all_states),str(all_messages),gens,runs,str(mutual_exclusivity)), 'a'))
     else: 
-        f_mean = csv.writer(open('experiments/%s/results/00mean-%s-s%s-m%s-g%d-r%d-me%s.csv' %(result_path, kind,str(states),str(messages),gens,runs,str(mutual_exclusivity)), 'w'))
+        f_mean = csv.writer(open('experiments/%s/results/00mean-%s-s%s-m%s-g%d-r%d-me%s.csv' %(result_path, kind,str(all_states),str(all_messages),gens,runs,str(mutual_exclusivity)), 'w'))
         f_mean.writerow(['kind','lam','alpha','k','samples','l','gens','runs','m_excl'] + ['t_mean'+str(x) for x in range(len(typeList))])
        
 
@@ -225,7 +220,7 @@ def run_dynamics(alpha,lam,k,sample_amount,gens,runs,learning_parameter,kind,mut
         end_results.append(f"{(get_lexica_representations(typ, lexica))}\n")
     # print("# All bins:", bins)
     
-    print("Finished:\t\t\t", datetime.datetime.now().replace(microsecond=0))
+    print("# Finished:\t\t\t", datetime.datetime.now().replace(microsecond=0))
     for line in end_results:
         print(line)
     with open(f"experiments/{result_path}/results/end_results.txt", "w") as end: