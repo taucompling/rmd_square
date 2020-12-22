6ea964203ea19cf4c3e007f9b1d78edd197c88c6
Maike Zuefle <maike@r-zuefle.de>
2020-12-22 12:26:01 +0200

    updated cost calculation

--- py_scripts/lexica.py
+++ py_scripts/lexica.py
@@ -134,7 +134,7 @@ def normalize_cost(cost_dict, all_states):
     for state in all_states:
         if state not in k_length_cost:
             raise Exception(f"No cost function defined for {state} states. Add it in lexica.py in get_prior().")
-
+    
     for key, value in cost_dict.items():
         cost_dict[key] = (max(k_length_cost[len(key)]) - value + 1)/np.sum(k_length_cost[len(key)])
 
@@ -162,14 +162,19 @@ def get_prior(lexica_list, cost, all_states, puzzle):
             (0,1):1, (1,0):1}  # for two states
 
     elif cost == "sonstiges":
-        cost_dict = {(0,0,1): 16, (0,1,1): 4, (1,1,0): 8, (1,0,0): 16,  # 4 corners
+        cost_dict = {(0,0,1): 7, (0,1,1): 7, (1,1,0):10, (1,0,0): 8,  # 4 corners
                     (1,0, 1): 20, (0,1,0): 20, # high
                     (0,1):1, (1,0):1}  # for two states
     else:
         raise Exception("Cost function is not defined. Please define it in lexica.py")
 
 
+    if puzzle:
+        del cost_dict[(1, 0, 1)]
+        del cost_dict[(0, 1, 0)]
+
     cost_dict = normalize_cost(cost_dict, all_states)
+
     out = []
     for lex in lexica_list:
         current_lex = np.transpose(lex)
