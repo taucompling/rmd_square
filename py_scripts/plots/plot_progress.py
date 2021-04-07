76308d98e8792ccef8186bd965c363c45ef5ca1e
zuefle <zuefle@cl.uni-heidelberg.de>
2021-04-07 20:37:59 +0200

    Better representation of results

--- py_scripts/plots/plot_progress.py
+++ py_scripts/plots/plot_progress.py
@@ -6,6 +6,7 @@ def plot_progress(probs, print_x,result_path):
     best_x = np.argpartition(probs[0], -print_x)[-print_x:]
 
     epochs_best_x = [[] for _ in range(len(best_x))]
+
     for run in probs:
         for t, prob in enumerate(run):
             if t in best_x:
@@ -14,7 +15,7 @@ def plot_progress(probs, print_x,result_path):
     for type, probs in enumerate(epochs_best_x):
         plt.plot(probs, label=f"Type: {type}")
     plt.ylabel("proportion")
-    plt.xlabel("epochs")
+    plt.xlabel("generations")
     plt.legend(best_x)
     plt.savefig(f'experiments/{result_path}/results/progress_best_{len(best_x)}_types.png')
     
--- py_scripts/plots/proportion_target_types.py
+++ py_scripts/plots/proportion_target_types.py
@@ -1,15 +1,40 @@
 import matplotlib.pyplot as plt
-def get_target_types(best, bins,  target_bins, competitor_bins, p_mean, result_path):
+import numpy as np
+
+def get_target_types(best, bins,  target_bins, competitor_bins, all_progress, result_path):
+    #f"# Average Proportion: {round(bin_orders[bin_winner][0]/bin_orders[bin_winner][1],4)}\n",
+
+    print("TARGET", target_bins)
+    print("COMPETITOR", competitor_bins)
+
     plt.clf()
-    target_types = ["T" + str(i+1) for i in range(len(["T" for bin in target_bins for typ in bins[bin]]))]
-    target_proportions = [p_mean[typ] for bin in target_bins for typ in bins[bin]]
+    target_types = bins[target_bins[0]]
+    competitor_types = bins[competitor_bins[0]]
+    runs = len(all_progress)
+    target_probs, competitor_probs =  [[] for _ in range(runs)], [[] for _ in range(runs)]
+    print(target_types)
+
+    for run_index, run in enumerate(all_progress):
+        last_generation = run[-1]
+        for t, prob in enumerate(last_generation):            
+            if t in target_types:
+                target_probs[run_index].append(prob)
+            if t in competitor_types:
+                competitor_probs[run_index].append(prob)
+
+
+    average_prob_target = np.average([np.max(prob) for prob in target_probs])
+    average_prob_comp = np.average([np.max(prob) for prob in competitor_probs])
+
+    target_types = [str(target_types)]
+    target_proportions = [average_prob_target]
     plt.bar(target_types,target_proportions, color="maroon", label="Target Type")
 
-    comp_types = ["C" + str(i+1) for i in range(len(["C" for bin in competitor_bins for typ in bins[bin]]))]
-    comp_proportions = [p_mean[typ] for bin in competitor_bins for typ in bins[bin]]
+    comp_types = [str(competitor_types)]
+    comp_proportions = [average_prob_comp]
     plt.bar(comp_types,comp_proportions, color="blue", label="Competitor Type")
 
-    plt.axhline(y=p_mean[best],linewidth=1, color='k', linestyle="dashed",  label="Majority type")
+    plt.axhline(y=best,linewidth=1, color='k', linestyle="dashed",  label="Majority type")
     plt.ylabel("proportion")
     plt.xlabel("types")
     plt.legend()
